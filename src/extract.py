8fa2f7e359e6c68ac1c701a7a6f71d7a56a4d777
Sebastian Wild <github@ignis.slmail.me>
2026-09-18 15:07:37 -0600

    Creating entries into the locations table when the database does not contain the location for the location

--- src/extract.py
+++ src/extract.py
@@ -1,7 +1,15 @@
 import psycopg2
+import os
+import json
 from config import parseConfig
 
 def main():
+    jsonObj = None
+    path = os.path.join(os.path.dirname(__file__), '..\\json\\archive.json')
+    print(path)
+    with open(path, 'r') as file:
+        content = file.read()
+        jsonObj = json.loads(content)
 
     conn = None
     try:
@@ -23,6 +31,13 @@ def main():
         print('Postgre SQL database version: ')
         print(db_version)
 
+        if(checkLocationExists(cur, jsonObj['latitude'],jsonObj['longitude'])):
+            print('Location Exists')
+        else:
+            if(jsonObj is not None):
+                print('Location does not exist')
+                addLocation(conn, cur, float(jsonObj['latitude']), float(jsonObj['longitude']), float(jsonObj['elevation']), jsonObj['timezone'])
+
         # close the communication with the PostgreSQL database
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
@@ -31,6 +46,14 @@ def main():
         if conn is not None:
             conn.close()
             print('Database connection closed.')
+
+def checkLocationExists(dbCursor, latitude, longitude) -> bool:
+    dbCursor.execute("SELECT LocationID, cityname FROM Locations WHERE Latitude = {0} AND Longitude = {1}".format(latitude, longitude))
+    return dbCursor.fetchone() != None
+
+def addLocation(dbConnection, dbCursor, latitude, longitude, elevation, timezone):
+    dbCursor.execute('INSERT INTO locations (latitude, longitude, elevation, timezone) VALUES (%s, %s, %s, %s);', (latitude, longitude,elevation,timezone))
+    dbConnection.commit()
 
 if __name__ == "__main__":
     main()
